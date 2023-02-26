571c6b5d9db59e8661259b4e7ec587449c59c6c8
Kisekio <dekostump@gmail.com>
2023-02-26 17:04:06 -0500

    Con cancion de zelda

--- C#2/escuela/Program.cs
+++ C#2/escuela/Program.cs
@@ -5,7 +5,26 @@
         public int añoFundacion;
         public string ceo;
         public void Timbrar(){
-            Console.Beep(424,3000);
+            Console.Beep(987, 1000); //Si
+            Console.Beep(1174, 500); //Re'
+            Console.Beep(880, 1500); //La
+
+            Console.Beep(783, 250); //Sol
+            Console.Beep(880, 250); //La
+            Console.Beep(987, 1000); //Si
+
+            Console.Beep(1174, 500); //Re'
+            Console.Beep(880, 1500); //La
+
+            Console.Beep(987, 1000); //Si
+            Console.Beep(1174, 500); //Re'
+            Console.Beep(1760, 1000); //La'
+            Console.Beep(1567, 500); //Sol'
+            Console.Beep(1174, 1000); //Re'
+
+            Console.Beep(1046, 250); //Do
+            Console.Beep(987, 250); //Si
+            Console.Beep(880, 1000);
         }
     }
 
