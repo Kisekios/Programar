4a1816b8a31a7748a7eccb18598fd39aa1bc2fed
Kisekio <dekostump@gmail.com>
2023-02-26 16:52:26 -0500

    Primer objeto

---
+++ C#2/escuela/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace escuela{
+    class Escuela{
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo;
+        public void Timbrar(){
+            Console.Beep(424,3000);
+        }
+    }
+
+    class Program{
+        static void Main (string[] args){
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Deko Academy";
+            miEscuela.direccion = "Centro";
+            miEscuela.añoFundacion = 2020;
+            Console.WriteLine("Timbre");
+            miEscuela.Timbrar();
+        }
+    }
+}
+
+
